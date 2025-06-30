bbdd52319e07cdba1d21a14996ea23d53b237194
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2025-06-30 10:33:51 -0400

    Updates

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,34 +12,38 @@ function Footer() {
   console.log(location.pathname);
   return (
     <footer className={styles.footer}>
-      <header>
+      <header className={styles.header}>
         <img src="/assets/icon.png" />
         <h1>D E S I G N O</h1>
       </header>
 
       <nav className={styles.nav}>
-        <Link to="/about">
-          <p onClick={scrollToTop}>OUR COMPANY</p>
+        <Link to="/about" onClick={scrollToTop}>
+          OUR COMPANY
         </Link>
-        <Link to="/locations">
-          <p onClick={scrollToTop}>LOCATIONS</p>
+        <Link to="/locations" onClick={scrollToTop}>
+          LOCATIONS
         </Link>
-        <Link to="/contact">
-          <p onClick={scrollToTop}>CONTACT</p>
+        <Link to="/contact" onClick={scrollToTop}>
+          CONTACT
         </Link>
       </nav>
 
-      <div className={styles.address}>
-        <h4>Designo Central Office</h4>
-        <p>3886 Wellington Street</p>
-        <p>Toronto, Ontario M9C 3J5</p>
-      </div>
+      <span className={styles.griditem1}>
+        <div className={styles.address}>
+          <h4>Designo Central Office</h4>
+          <p>3886 Wellington Street</p>
+          <p>Toronto, Ontario M9C 3J5</p>
+        </div>
+      </span>
 
-      <div className={styles.address}>
-        <h4>Contact Us (Central Office)</h4>
-        <p>P: 1+ 253-863-8967</p>
-        <p>M: contact@designo.co</p>
-      </div>
+      <span className={styles.griditem2}>
+        <div className={styles.address}>
+          <h4>Contact Us (Central Office)</h4>
+          <p>P: 1+ 253-863-8967</p>
+          <p>M: contact@designo.co</p>
+        </div>
+      </span>
 
       <div className={styles.icons}>
         <img src="/assets/social-media-icons.png" />
